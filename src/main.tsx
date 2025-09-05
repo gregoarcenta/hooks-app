07b9e8fa5a050c91109eccb3b140129f074308a7
Gregory Arcentales <gregoarcenta@gmail.com>
2025-09-04 22:24:02 -0500

    Implement TasksApp with useReducer for task management and integrate it into the root component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import FocusScreen from "./useRef/FocusScreen.tsx";
+import { TasksApp } from "./useReducer/TasksApp.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -10,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
     {/*<TrafficLightWithEffect />*/}
     {/*<TrafficLightWithHook />*/}
     {/*<PokemonPage />*/}
-    <FocusScreen />
+    {/*<FocusScreen />*/}
+    <TasksApp />
   </StrictMode>,
 );
